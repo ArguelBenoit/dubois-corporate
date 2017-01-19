6524ec6962ea3c6b63914c774d6b9dff33a02a39
BenoitArguel <benoit.arguel@gmail.com>
2017-01-19 11:34:36 +0100

    task: add homepage cover

--- js/style.js
+++ js/style.js
@@ -6,11 +6,20 @@ function allSizeCss() {
 
   ['#homepage', '#info'].forEach(function(element){
     $(element + ' .container').css('padding-top', ( window.innerHeight - $(element + ' .container').height() ) / 2 + 'px');
+    // if ($(element).height() > window.innerHeight) {
+    //   $(element).css('padding-top',  '70px');
+    //   $(element).css('padding-bottom',  '70px');
+    // }
   });
 
-  $('.container2arrow').css('left', ( ( window.innerWidth / 2 ) - 28 ) + 'px');
+  $('.container2arrow').css('left', ( ( window.innerWidth / 2 ) - 20 ) + 'px');
   ['#container2arrow1', '#container2arrow2', '#container2arrow3'].forEach(function(element, i){
-    $(element).css('top', ( ( window.innerHeight * (i+1) ) - 72 ) + 'px');
+    if (i+1 == 1)
+      $(element).css('top', ( $('#homepage').height() - 64 ) + 'px');
+    if (i+1 == 2)
+      $(element).css('top', ( $('#homepage').height() + $('#info').height() - 64 ) + 'px');
+    if (i+1 == 3)
+      $(element).css('top', ( $('#homepage').height() + $('#info').height() + $('#map').height() - 64 ) + 'px');
   });
 }
 
